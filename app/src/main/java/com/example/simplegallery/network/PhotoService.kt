84ee9c42a55462e3aa8a6638cc10b7dcf8a00a7e
onats1 <donatemowo@yahoo.com>
2025-07-23 01:51:02 +0100

    Reverting to original state.

--- app/src/main/java/com/example/simplegallery/network/PhotoService.kt
+++ app/src/main/java/com/example/simplegallery/network/PhotoService.kt
@@ -3,7 +3,6 @@ package com.example.simplegallery.network
 import com.example.simplegallery.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Headers
-import retrofit2.http.Query
 
 //Change 1:Use API_KEY from build config
 const val API_KEY = "w3RfaqXkhVmuodyyszutfM1dN-hy3v7m2t-9SA3klkc"
@@ -11,9 +10,6 @@ const val API_KEY = "w3RfaqXkhVmuodyyszutfM1dN-hy3v7m2t-9SA3klkc"
 interface PhotoService {
     //Change 2: We can move this header to the okHttp class in the retrofit builder
    // @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
-    @GET("photos")
-    suspend fun getPhotos(
-        @Query("page") page: Int,
-        @Query("per_page") perPage: Int = 50
-    ): List<PhotoRemote>
+    @GET("photos/?page=1&per_page=50")
+    suspend fun getPhotos(): List<PhotoRemote>
 }
